c70a8479a0b3190ad0a3a2d0891066892ab03989
Juan Carlos Farah <juancarlos.farah@pm.me>
2023-10-12 21:48:58 +0200

    feat: add typeorm boilerplate

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,29 @@
+import 'reflect-metadata'
 import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+
+import { AppDataSource } from './data-source'
+import { User } from './entity/User'
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log('Inserting a new user into the database...')
+    const user = new User()
+    user.firstName = 'Timber'
+    user.lastName = 'Saw'
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log('Saved a new user with id: ' + user.id)
+
+    console.log('Loading users from the database...')
+    const users = await AppDataSource.manager.find(User)
+    console.log('Loaded users: ', users)
+
+    console.log('Here you can setup and run express / fastify / any other framework.')
+  })
+  .catch((error) => console.log(error))
 
 function createWindow(): void {
   // Create the browser window.
